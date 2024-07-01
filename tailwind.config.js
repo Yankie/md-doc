ca0da870b896f5a25ac9441fffd18dc3e47ddeaf
Pavel Yefimov <me@yankie.ru>
2024-07-01 21:04:22 +0300

    Merging-up stashes #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       'sans': ['"PT Astra Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
       'serif': ['"PT Astra Serif"', 'ui-serif'],
       'mono': ['"PT Mono"', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
-  
+
       // 'display': ['"PT Astra Sans"', 'ui-sans-serif', 'sans-serif'],
       'display': ['"PT Astra Serif"', 'ui-serif'],
       'body': ['"PT Astra Sans"', 'ui-sans-serif', 'sans-serif'],
@@ -14,10 +14,11 @@ module.exports = {
     },
     extend: {},
   },
+  darkMode: ['class', '[data-theme="dark"]'],
+  // darkMode: 'selector',
   daisyui: {
     themes: ["light", "dark"],
   },
-  darkMode: ['class', '[data-theme="dark"]'],
   plugins: [
     require('@tailwindcss/typography'),
     require('daisyui'),
